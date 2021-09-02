7aba088bbcee5d75f5cfd397b799a7688b258d5c
chusj <chusj.mail@qq.com>
2021-09-02 14:59:12 +0800

    递归取数组的总数

---
+++ Algorithm/SortAlgorithm/SortAlgorithmConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SortAlgorithmConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int sum = Recursion.GetSum(arr, arr.Length);
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithm/SortAlgorithm/SortAlgorithmConsoleApp/Recursion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithmConsoleApp
+{
+    /// <summary>
+    /// 递归
+    /// </summary>
+    public class Recursion
+    {
+        /// <summary>
+        /// （递归）获取总数
+        /// </summary>
+        /// <param name="array">数组</param>
+        /// <param name="length">长度</param>
+        /// <returns></returns>
+        public static int GetSum(int[] array, int length)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int n = length - 1;
+                if (n == 0)
+                {
+                    return array[n];
+                }
+                else
+                {
+                    return array[n] + GetSum(array, length - 1);
+                }
+            }
+        }
+    }
+}
