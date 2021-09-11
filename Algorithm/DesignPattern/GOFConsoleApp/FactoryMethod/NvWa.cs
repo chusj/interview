2876cfd03b1d2768c108f3b5ebc966f1cfab158a
chusj <chusj.mail@qq.com>
2021-09-11 23:19:12 +0800

    加注释，移除using

--- Algorithm/DesignPattern/GOFConsoleApp/FactoryMethod/NvWa.cs
+++ Algorithm/DesignPattern/GOFConsoleApp/FactoryMethod/NvWa.cs
@@ -16,9 +16,10 @@ namespace GOFConsoleApp.FactoryMethod
         /// </summary>
         public void SimpleFactory()
         {
+            //优点：将对象的创建交给专门的工厂类负责，实现了对象的创建和对象的使用分离。
             HumanFacotry facotry = new HumanFacotry();
             Console.WriteLine("女娲开始造人了，时间太久……");
-            IHuman human = facotry.CreateHuman(1);
+            IHuman human = facotry.CreateHuman(1); //缺点：工厂类不够灵活，增加新的具体产品需要修改工厂类的判断逻辑代码
             human.SkinColor();
             human.Say();
 
